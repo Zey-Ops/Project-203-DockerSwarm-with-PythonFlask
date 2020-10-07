c552212d2d1e74e581247150a8240c7b640243f7
zylina <zylina.lion@gmail.com>
2020-10-07 12:00:28 -0400

    updated html files

--- init.sql
+++ init.sql
@@ -1,11 +1,17 @@
-CREATE TABLE phonebook_db.phonebook(
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(100) NOT NULL,
-    number VARCHAR(100) NOT NULL,
-    PRIMARY KEY (id)
+CREATE TABLE phonebook_db.phonebook
+(
+    id INT NOT NULL
+    AUTO_INCREMENT,
+    name VARCHAR
+    (100) NOT NULL,
+    number VARCHAR
+    (100) NOT NULL,
+    PRIMARY KEY
+    (id)
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-INSERT INTO phonebook_db.phonebook (name, number)
+    INSERT INTO phonebook_db.phonebook
+        (name, number)
     VALUES
         ("Callahan", "1234567890"),
         ("Jason O", "12345678"),
